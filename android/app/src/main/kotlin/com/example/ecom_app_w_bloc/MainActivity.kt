5848e8b8bf9498df2d560ce20dc9541aef1e7e3e
Spikree <avimahalingpure10@gmail.com>
2024-01-17 18:44:47 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/ecom_app_w_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ecom_app_w_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,52 @@
+class GroceryData {
+  static List<Map<String, dynamic>> groceryProducts = [
+    {
+      'name': 'Apple',
+      'category': 'Fruits',
+      'price': 1.99,
+      'quantity': 10,
+      'imageUrl':
+          'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit.jpg',
+    },
+    {
+      'name': 'Bread',
+      'category': 'Bakery',
+      'price': 2.49,
+      'quantity': 2,
+      'imageUrl':
+          'https://www.backerhausveit.com/wp-content/uploads/2021/03/17783-1.jpg',
+    },
+    {
+      'name': 'Milk',
+      'category': 'Dairy',
+      'price': 1.79,
+      'quantity': 1,
+      'imageUrl':
+          'https://www.dairyglobal.net/app/uploads/2023/02/IMG_Milkinglassbottles_canva_WEB.jpg',
+    },
+    {
+      'name': 'Eggs',
+      'category': 'Dairy',
+      'price': 3.99,
+      'quantity': 12,
+      'imageUrl':
+          'https://www.mayoclinichealthsystem.org/-/media/national-files/images/hometown-health/2021/eggs-in-a-wood-bowl.jpg?sc_lang=en',
+    },
+    {
+      'name': 'Chicken Breast',
+      'category': 'Meat',
+      'price': 5.99,
+      'quantity': 2,
+      'imageUrl':
+          'https://www.thespruceeats.com/thmb/vOhouyHgfcn7DZ-ScX0YOD6T_y0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1127568336-ff8bd2c0f5df42a89a510b39389d9e9c.jpg',
+    },
+    {
+      'name': 'Tomato',
+      'category': 'Vegetables',
+      'price': 0.99,
+      'quantity': 5,
+      'imageUrl':
+          'https://seed2plant.in/cdn/shop/products/tomatoseeds.jpg?v=1604033216',
+    },
+  ];
+}
